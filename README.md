dfbdec50162feabfb85773180ac3150fc9affe2d
Alexey Samara <lion.samara@gmail.com>
2017-12-17 18:01:26 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# symfony-packagist
+![Symfony Packagist Banner](http://cdn.wow-apps.pro/packagist/symfony-packagist-banner-v2.png)
+
+# Symfony Packagist API Bundle
 Packagist API Bundle for Symfony 3 and 4
---
+++ Service/Packagist.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of the Symfony-Bundles.com project
+ * https://github.com/wow-apps/symfony-bundles
+ *
+ * (c) 2017 WoW-Apps
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WowApps\PackagistBundle\Service;
+
+/**
+ * Class Packagist
+ *
+ * @author Alexey Samara <lion.samara@gmail.com>
+ * @package wow-apps/symfony-packagist
+ */
+class Packagist
+{
+
+}
