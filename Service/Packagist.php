1ecfbce9e00515868de1b694c85efa282a17440b
Alexey Samara <lion.samara@gmail.com>
2017-12-17 20:47:52 +0200

    1.0.0 (view CHANGELOG.md)

--- Service/Packagist.php
+++ Service/Packagist.php
@@ -11,6 +11,9 @@
 
 namespace WowApps\PackagistBundle\Service;
 
+use GuzzleHttp\Client as GuzzleClient;
+use WowApps\PackagistBundle\DTO\Package;
+
 /**
  * Class Packagist
  *
@@ -19,5 +22,50 @@ namespace WowApps\PackagistBundle\Service;
  */
 class Packagist
 {
+    const API_URL = 'https://packagist.org';
+    const API_URL_LIST = self::API_URL . '/packages/list.json';
+    const API_URL_SEARCH = self::API_URL . '/packages/search.json';
+    const API_URL_PACKAGE = self::API_URL . '/packages/%s.json';
 
+    /** @var GuzzleClient */
+    private $guzzleClient;
+
+    /**
+     * Packagist constructor.
+     */
+    public function __construct() {
+        $this->guzzleClient = new GuzzleClient();
+    }
+
+    /**
+     * @param string $packageName
+     * @return Package
+     */
+    public function getPackage(string $packageName): Package
+    {
+        //
+    }
+
+    /**
+     * @param string $url
+     * @return array
+     * @throws \RuntimeException
+     */
+    private function getAPIResponse(string $url): array
+    {
+        try {
+            $request = $this->guzzleClient->get($url);
+        } catch (ClientException $e) {
+            throw new \RuntimeException($e->getMessage());
+        }
+
+        $response = $request->getBody();
+        $json = json_decode($response, true);
+
+        if (!$json) {
+            throw new \RuntimeException('Can\'t parse json');
+        }
+
+        return $json;
+    }
 }
