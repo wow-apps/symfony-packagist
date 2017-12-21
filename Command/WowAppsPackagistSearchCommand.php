8f0cf9e5ca216a3da5cae60bc0046b207313e8fb
Alexey Samara <alexey.samara@intexsys.lv>
2017-12-21 19:31:13 +0200

    1.1.0 (view changelog)

---
+++ Command/WowAppsPackagistSearchCommand.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * This file is part of the wow-apps/symfony-packagist project
+ * https://github.com/wow-apps/symfony-packagist
+ *
+ * (c) 2017 WoW-Apps
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WowApps\PackagistBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use WowApps\PackagistBundle\Exception\PackagistException;
+use WowApps\PackagistBundle\Service\Packagist;
+
+/**
+ * Class WowAppsPackagistSearchCommand
+ *
+ * @author Alexey Samara <lion.samara@gmail.com>
+ * @package wow-apps/symfony-packagist
+ */
+class WowAppsPackagistSearchCommand extends ContainerAwareCommand
+{
+    /**
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('wowapps:packagist:search')
+            ->setDescription('Search for packages')
+            ->addArgument('search_query', InputArgument::OPTIONAL, 'Search query')
+            ->addOption(
+                'tag',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'You can set package tag to search'
+            )
+            ->addOption(
+                'type',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'You can set type of packages to search'
+            )
+        ;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /** @var Packagist $packagist */
+        $packagist = $this->getContainer()->get('wowapps.packagist.service');
+        $searchQuery = strtolower($input->getArgument('search_query'));
+        $symfonyStyle = new SymfonyStyle($input, $output);
+
+        echo PHP_EOL;
+        $output->writeln('<bg=green;options=bold;fg=white>                                               </>');
+        $output->writeln('<bg=green;options=bold;fg=white>         Symfony Packagist API Bundle          </>');
+        $output->writeln('<bg=green;options=bold;fg=white>                                               </>');
+        echo PHP_EOL;
+
+        if (empty($searchQuery)) {
+            $symfonyStyle->error([
+                PackagistException::E_EMPTY_SEARCH_QUERY,
+                PackagistException::E_EMPTY_SEARCH_QUERY_DESCRIPTION,
+            ]);
+
+            return;
+        }
+
+        $symfonyStyle->title('Search for packages');
+
+        $packagesList = $packagist->searchPackages(
+            $searchQuery,
+            $input->getOption('tag'),
+            $input->getOption('type')
+        );
+
+        if (!$packagesList->count()) {
+            $symfonyStyle->warning(PackagistException::W_NO_SEARCH_RESULT);
+            return;
+        }
+
+        $showList = $symfonyStyle->confirm(
+            sprintf('Founded %d packages. Do you want to show them all?', $packagesList->count()),
+            false
+        );
+
+        if ($showList) {
+            foreach ($packagesList as $package) {
+                $output->writeln(
+                    sprintf(
+                        '<options=bold;fg=yellow>%s</>%s',
+                        $package->getName(),
+                        PHP_EOL . $package->getDescription() . PHP_EOL
+                    )
+                );
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,7 +1,69 @@
 ![Symfony Packagist Banner](http://cdn.wow-apps.pro/packagist/symfony-packagist-banner-v2.png)
 
+[![SensioLabsInsight](https://insight.sensiolabs.com/projects/9e427ba8-ceee-47a4-aeef-a788b9875064/big.png)](https://insight.sensiolabs.com/projects/9e427ba8-ceee-47a4-aeef-a788b9875064)
+
+[![Packagist](https://img.shields.io/packagist/v/wow-apps/symfony-packagist.svg?style=flat-square)](https://github.com/wow-apps/symfony-packagist)
+[![Packagist](https://img.shields.io/packagist/dt/wow-apps/symfony-packagist.svg?style=flat-square)](https://packagist.org/packages/wow-apps/symfony-packagist)
+[![Build Status](https://scrutinizer-ci.com/g/wow-apps/symfony-packagist/badges/build.png?b=master)](https://scrutinizer-ci.com/g/wow-apps/symfony-packagist/build-status/master)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/wow-apps/symfony-packagist/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/wow-apps/symfony-packagist/?branch=master)
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)](https://github.com/wow-apps/symfony-packagist/blob/master/LICENCE)
+[![Code Climate](https://codeclimate.com/github/wow-apps/symfony-packagist/badges/gpa.svg)](https://codeclimate.com/github/wow-apps/symfony-packagist)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/ce3fffd811f2463a94ed4065a341885a)](https://www.codacy.com/app/lion-samara/symfony-packagist?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=wow-apps/symfony-packagist&amp;utm_campaign=Badge_Grade)
+[![SensioLabsInsight](https://insight.sensiolabs.com/projects/9e427ba8-ceee-47a4-aeef-a788b9875064/mini.png)](https://insight.sensiolabs.com/projects/9e427ba8-ceee-47a4-aeef-a788b9875064)
+
+
 # Symfony Packagist API Bundle
+
 Packagist API Bundle for Symfony 3 and 4
 
+## Installation:
+
+### Requires:
+
+* PHP 7.0+
+* Symfony 3.0+
+* Guzzle Client 6.0+
+
+### Step 1: Download the Bundle
+
+```json
+"require": {
+        "wow-apps/symfony-packagist": "^1"
+}
+```
+
+or
+
+```bash
+$ composer require wow-apps/symfony-packagist 
+```
+
+### Step 2: Enable the Bundle (skip for Symfony 4)
+
+```php
+// ./app/AppKernel.php
+
+public function registerBundles()
+{
+    $bundles = array(
+        // ...
+        new WowApps\PackagistBundle\WowAppsPackagistBundle(),
+    );
+
+    // ...
+
+    return $bundles
+}
+```
+
+# Documentation
+
+* [Installation](https://github.com/wow-apps/symfony-packagist/wiki/1_Installation)
+* [Commands](https://github.com/wow-apps/symfony-packagist/wiki/2.-Commands)
+* [Using Packagist API Bundle](https://github.com/wow-apps/symfony-packagist/wiki/3.-Using-Packagist-API-Bundle)
+    
+# News and updates:
+
+Follow news and updates in my Telegram channel [@wow_apps_pro](https://t.me/wow_apps_pro) or Twitter [@alexey_samara_](https://twitter.com/alexey_samara_)
 
 ![Symfony Packagist Preview](http://cdn.wow-apps.pro/packagist/symfony-packagist-preview.jpg)
